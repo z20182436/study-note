c5f3b54830a72518fb17089ba1b7f6d9abe668f9
unknown <zouzhonghang_zd@163.com>
2020-08-10 18:28:25 +0800

    Github 第一次提交

---
+++ src/main/java/com/example/studynotes/StudyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.studynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyNotesApplication.class, args);
+	}
+
+}
